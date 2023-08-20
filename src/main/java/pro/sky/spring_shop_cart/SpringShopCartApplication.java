8c44295d3f21752cd3479c5336f21527c4d8bea4
Anna <anna051977@gmail.com>
2023-08-20 19:27:00 +0300

    Initial commit

---
+++ src/main/java/pro/sky/spring_shop_cart/SpringShopCartApplication.java
@@ -0,0 +1,12 @@
+package pro.sky.spring_shop_cart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringShopCartApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringShopCartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/spring_shop_cart/SpringShopCartApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.spring_shop_cart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringShopCartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
