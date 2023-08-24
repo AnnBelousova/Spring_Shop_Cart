f575821bc38809440bb51b873cd1e344b5aea16d
Anna <anna051977@gmail.com>
2023-08-24 12:39:34 +0300

    Third commit

--- src/main/java/pro/sky/spring_shop_cart/services/impl/ServiceCartImpl.java
+++ src/main/java/pro/sky/spring_shop_cart/services/impl/ServiceCartImpl.java
@@ -1,13 +1,14 @@
 package pro.sky.spring_shop_cart.services.impl;
 
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Service;
 import pro.sky.spring_shop_cart.services.ServiceCart;
 
 import java.util.*;
 
 @Service
-@Scope("prototype")
+@Scope(value = "session", proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class ServiceCartImpl implements ServiceCart {
     List<Integer> cart = new ArrayList<>();
 
